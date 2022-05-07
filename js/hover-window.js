aad37521cbe371c13349165881c712e38640e634
S-L-I-G-H-T-N-I-N-G <89953982+S-L-I-G-H-T-N-I-N-G@users.noreply.github.com>
2022-05-07 10:37:28 +0800

    优化显示方式

--- js/hover-window.js
+++ js/hover-window.js
@@ -1,5 +1,5 @@
 function showImg(src) {
-    document.getElementById("hover-window--img").src = src
+    document.getElementById("hover-window--img").style.backgroundImage = "url(" + src + ")"
     document.getElementById("hover-window-hover").style.display = "block"
 }
 function closeHoverWindow() {
