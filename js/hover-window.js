7b9fc8e5f361682184ad5bbea79483955c8215be
S-L-I-G-H-T-N-I-N-G <89953982+S-L-I-G-H-T-N-I-N-G@users.noreply.github.com>
2022-05-07 18:12:29 +0800

    悬浮窗有了动画效果

--- js/hover-window.js
+++ js/hover-window.js
@@ -1,8 +1,14 @@
 function showHoverWindow(title, src) {
     document.getElementById("hover-window--title").innerText = title
     document.getElementById("hover-window--img").style.backgroundImage = "url(" + src + ")"
-    document.getElementById("hover-window--hover").style.display = "block"
+    var hoverWindowHover = document.getElementById("hover-window--hover").style
+    hoverWindowHover.pointerEvents = "auto"
+    hoverWindowHover.opacity = 1
+    document.getElementById("hover-window").style.transform = "scale(1)"
 }
 function closeHoverWindow() {
-    document.getElementById("hover-window--hover").style.display = "none"
+    var hoverWindowHover = document.getElementById("hover-window--hover").style
+    hoverWindowHover.pointerEvents = "none"
+    hoverWindowHover.opacity = 0
+    document.getElementById("hover-window").style.transform = "scale(0.5)"
 }
