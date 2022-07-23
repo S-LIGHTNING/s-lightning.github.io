a66b68943eec285b58623d7fc09557678a761e88
SLIGHTNING <3157420882@qq.com>
2022-07-23 16:50:45 +0800

    修复滚动 BUG，按钮特效 BUG，增加广告的导航按钮

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+function getHeightToTop(ele){
+    let root = document.body;
+    let height = 0;
+    do {
+        height += ele.offsetTop;
+        ele = ele.offsetParent;
+    } while( ele != root )
+    return height;
+}
+
+function scrollToEle(id) {
+    var top = getHeightToTop(document.getElementById(id))
+    var topEle;
+    if (innerWidth < 512) topEle = "nav-bar"
+    else topEle = "info"
+    document.documentElement.scrollTop = top - document.getElementById(topEle).offsetHeight
+}
